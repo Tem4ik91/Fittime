2d44ca88835531c04c0d6df9557b1889c1b36505
Tem4ik91 <purgen-666@ya.ru>
2021-01-17 03:35:11 +0300

    sidebar

--- app/src/main/java/com/example/fittime/MainActivity.kt
+++ app/src/main/java/com/example/fittime/MainActivity.kt
@@ -2,14 +2,69 @@ package com.example.fittime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.fittime.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mDrawer: Drawer
+    private lateinit var mToolbar: Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+       setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Профиль")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+       mHeader = AccountHeaderBuilder()
+           .withActivity(this)
+           .withHeaderBackground(R.drawable.header)
+           .addProfiles(
+               ProfileDrawerItem().withName("вапаа").withEmail("+7111111")
+           ).build()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
     }
 }
