818030b6d8a4fad17bad1d5200f37e3675966e08
Tem4ik91 <purgen-666@ya.ru>
2021-01-30 11:26:46 +0300

    menu Exit

--- app/src/main/java/com/example/fittime/ui/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/fittime/ui/Fragment/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.example.fittime.ui.Fragment
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.fittime.R
 
 
@@ -8,7 +10,10 @@ class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
 
-
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 }
