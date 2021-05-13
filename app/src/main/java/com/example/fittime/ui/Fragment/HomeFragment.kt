55d436d59c5b85245802d17fd88e5fbf9630d497
Tem4ik91 <purgen-666@ya.ru>
2021-05-13 15:23:35 +0300

    home fragment

--- app/src/main/java/com/example/fittime/ui/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/fittime/ui/Fragment/HomeFragment.kt
@@ -2,6 +2,12 @@ package com.example.fittime.ui.Fragment
 
 import androidx.fragment.app.Fragment
 import com.example.fittime.R
+import com.example.fittime.utlits.APP_ACTIVITY
+import com.example.fittime.utlits.USER
+import com.example.fittime.utlits.downloadAndSetImage
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -9,5 +15,16 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onResume() {
         super.onResume()
+      //  setHasOptionsMenu(true)
+        initFields()
     }
+
+    private fun initFields() {
+
+        settings_full_name.text = USER.fullname
+
+        settings_profile_image.downloadAndSetImage(USER.photoUrl)
+
+    }
+
 }
