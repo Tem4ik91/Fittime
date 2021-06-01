f75c9a02ea520a0f59747bfa2bc3f6832a626a0e
Tem4ik91 <purgen-666@ya.ru>
2021-06-02 00:19:30 +0300

    api fit google acc

--- app/src/main/java/com/example/fittime/ui/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/fittime/ui/Fragment/HomeFragment.kt
@@ -1,30 +1,120 @@
 package com.example.fittime.ui.Fragment
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.helper.widget.MotionEffect.TAG
 import androidx.fragment.app.Fragment
 import com.example.fittime.R
 import com.example.fittime.utlits.APP_ACTIVITY
 import com.example.fittime.utlits.USER
 import com.example.fittime.utlits.downloadAndSetImage
-import com.theartofdev.edmodo.cropper.CropImage
-import com.theartofdev.edmodo.cropper.CropImageView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.request.DataReadRequest
 import kotlinx.android.synthetic.main.fragment_profile.*
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.concurrent.TimeUnit
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
 
+    val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1
+    val fitnessOptions = FitnessOptions.builder()
+        .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+        .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+        .build()
+
     override fun onResume() {
         super.onResume()
       //  setHasOptionsMenu(true)
         initFields()
+
     }
 
     private fun initFields() {
 
-        settings_full_name.text = USER.fullname
 
+
+        settings_full_name.text = USER.fullname
         settings_profile_image.downloadAndSetImage(USER.photoUrl)
 
+        val fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .build()
+
+        val account = GoogleSignIn.getAccountForExtension(APP_ACTIVITY, fitnessOptions)
+
+        if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                APP_ACTIVITY, // your activity
+                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE, // e.g. 1
+                account,
+                fitnessOptions)
+        } else {
+            accessGoogleFit()
+        }
+
+
+        Fitness.getRecordingClient(APP_ACTIVITY, GoogleSignIn.getAccountForExtension(APP_ACTIVITY, fitnessOptions))
+            // This example shows subscribing to a DataType, across all possible data
+            // sources. Alternatively, a specific DataSource can be used.
+            .subscribe(DataType.TYPE_STEP_COUNT_DELTA)
+            .addOnSuccessListener {
+                Log.i(TAG, "Successfully subscribed!")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "There was a problem subscribing.", e)
+            }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> when (requestCode) {
+                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE -> accessGoogleFit()
+                else -> {
+                    // Result wasn't from Google Fit
+                }
+            }
+            else -> {
+                // Permission not granted
+            }
+        }
+    }
+
+    private fun accessGoogleFit() {
+        val end = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDateTime.now()
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+        val start = end.minusYears(1)
+        val endSeconds = end.atZone(ZoneId.systemDefault()).toEpochSecond()
+        val startSeconds = start.atZone(ZoneId.systemDefault()).toEpochSecond()
+
+        val readRequest = DataReadRequest.Builder()
+            .aggregate(DataType.AGGREGATE_STEP_COUNT_DELTA)
+            .setTimeRange(startSeconds, endSeconds, TimeUnit.SECONDS)
+            .bucketByTime(1, TimeUnit.DAYS)
+            .build()
+        val account = GoogleSignIn.getAccountForExtension(APP_ACTIVITY, fitnessOptions)
+        Fitness.getHistoryClient(APP_ACTIVITY, account)
+            .readData(readRequest)
+            .addOnSuccessListener ({ response ->
+                // Use response data here
+                Log.i(TAG, "OnSuccess()")
+            })
+            .addOnFailureListener({ e -> Log.d(TAG, "OnFailure()", e) })
+    }
+
 
 }
